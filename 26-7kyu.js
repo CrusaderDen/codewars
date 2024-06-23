7771092307778261b23672fec638c2a133c3d155
Denis Krestov <rupo@list.ru>
2024-06-23 21:45:42 +0300

    Add a new cata

---
+++ 26-7kyu.js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/5a4ff3c5fd56cbaf9800003e/train/javascript
+
+In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+Good luck!
+
+Hint: watch out for side effects.
+
+Some good reading: MDN Docs about arrays
+
+
+*/
+
+function withoutLast(arr) {
+   return arr.slice(0, -1)
+}
+
+console.log(withoutLast([1, 2, 3]))
